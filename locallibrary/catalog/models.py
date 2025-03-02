1fb954b2cfa594f0e8eb8099d3e20978de5c12cc
GrantDvo <168016702+GrantDvo@users.noreply.github.com>
2025-03-02 15:54:01 -0600

    Python Anywhere Push

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -6,6 +6,9 @@ from django.db import models
 from django.urls import reverse
 
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
@@ -16,7 +19,8 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', 'null=True, blank=True')
+    date_of_death = models.DateField('Died', null=True, blank=True)
+    author_image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
@@ -35,6 +39,7 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Characters <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    book_image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -59,6 +64,15 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+
+        return bool(self.due_back and date.today() >
+self.due_back)
 
     class Meta:
         ordering = ['due_back']
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4+swu)%n(ee5$9w%vx3)@bhhpqkkx$-7ozt3*_f5qq39$s27='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'register.apps.RegisterConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -130,3 +133,10 @@ import os
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'catalog/static'),
+]
